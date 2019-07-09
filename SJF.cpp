000ff1aae8dca2753acfc22d0f9232a3adf15e3d
Mohammed Alkandari <mohammedalkandari8@gmail.com>
2019-07-09 15:17:51 +0300

    added table displaying processes info

--- SJF.cpp
+++ SJF.cpp
@@ -8,20 +8,20 @@ cout<<"\n/////////////////////////////////";
 cout<<"\n/    Shortest Job First         /";
 cout<<"\n/////////////////////////////////";
 float t_totalTAT=0,t_totalWT=0;
-int n=5;
-int t=0;
+int n=7; // number of processes
+int t=0; // number of rounds
+
 for(t=0;t<3;t++){
 int i,j,min,line[10],line1[10],p[10]={1,2,3,4,5,6,7,8,9,10};
 int B_T[1000],A_V_T[1000],W_T[1000],temp,T_A_T[10];
 int bt=0,ta=0,sum=0,k=1;
 float AVR_W_T=0,AVR_T_A_T=0,T_A_T_SUM=0,W_T_SUM=0;
-cout<<"\n Turn Number "<<t+1;
 
+cout<<"\n Turn Number "<<t+1;
 for(i=0;i<n;i++)
 {
 A_V_T[i]=rand()%10;
 B_T[i]=rand()%10;
-
 }
 for(i=0;i<n;i++)
 {
@@ -83,6 +83,14 @@ T_A_T_SUM=T_A_T_SUM+T_A_T[i];
 
 AVR_T_A_T=(T_A_T_SUM/n);
 
+printf(":\n\n");
+printf("P#\t AT\t BT\t TAT\t WT\t\n\n");
+
+for(int i=0;i<n;i++)
+{
+printf("P%d\t %d\t %d\t %d\t %d\n",i+1,A_V_T[i],B_T[i],T_A_T[i],W_T[i]);
+}
+
 cout<<"\nAVERAGE W TIME :"<<AVR_W_T;
 cout<<"\nAVERAGE TA TIME :"<<AVR_T_A_T;
 t_totalTAT=t_totalTAT+AVR_T_A_T;
@@ -91,29 +99,6 @@ cout<<"\n--------------------";
 }
 cout<<"\n Average TA Time for all turns ="<<t_totalTAT/3;
 cout<<"\n Average WT for all turns = "<<t_totalWT/3;
+printf("\n\n");
 return 0;
-
 }
-
-/*
-
-output
-
------SJF----------
-Turn 1
-AVERAGE WAITING TIME :3.4
-AVERAGE TURN AROUND TIME :7.8
---------------------
-Turn 2
-AVERAGE WAITING TIME :4.4
-AVERAGE TURN AROUND TIME :8.4
---------------------
-Turn 3
-AVERAGE WAITING TIME :4.2
-AVERAGE TURN AROUND TIME :8
---------------------
-Average Turnaround Time for all 3 turn =8.06667
-Average WT for all 3 turn = 4
-
-*/
-
